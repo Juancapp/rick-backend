6b3a62810511fd4ed20a983284d47235748d2ead
Juancapp <juan.cappuccio@radiumrocket.com>
2023-08-31 16:43:40 -0300

    features/RM: set firebase middleware

--- src/routes/character/index.ts
+++ src/routes/character/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import controllers from "./controllers";
+import authMiddleware from "../../middlewares/firebase";
 
 const router = express.Router();
 
-router.route("/:sortby/:page").get(controllers.getCharacters);
+router.route("/:sortby/:page").get(authMiddleware, controllers.getCharacters);
 
 export default router;
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import controllers from "./controllers";
+import authMiddleware from "../../middlewares/firebase";
 
 const router = express.Router();
 
 router.route("/").post(controllers.createUser);
-router.route("/edit-favorite").patch(controllers.editFavoriteCharacter);
+router
+  .route("/edit-favorite")
+  .patch(authMiddleware, controllers.editFavoriteCharacter);
 
 export default router;
